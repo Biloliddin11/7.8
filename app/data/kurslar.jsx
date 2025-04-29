6bb6c6186e4b3ab755e5dfe69c68aa9928fd3141
Biloliddin11 <andreybralstars@gmail.com>
2025-04-29 07:05:42 -0700

    l

--- app/data/kurslar.jsx
+++ app/data/kurslar.jsx
@@ -4,33 +4,33 @@ export const kurslar = [
       id: 'react',
       nom: ' PRE-IELTS',
       rasm: '/react.jpg',
-      tavsif: ''
+      tavsif: '9:00 - 11:00 gacha'
     },
     {
       id: 'nextjs',
       nom: 'IELTS',
       rasm: '/nextjs.jpg',
-      tavsif: ''
+      tavsif: '11:00 - 13:00 gacha'
     } ,{
       id: 'nextjs',
       nom: 'RUS',
       rasm: '/nextjs.jpg',
-      tavsif: ''
+      tavsif: '14:00 - 16:00 gacha'
     } ,{
       id: 'nextjs',
       nom: 'TURKEY',
       rasm: '/nextjs.jpg',
-      tavsif: ''
+      tavsif: '9:00 - 11:00 gacha'
     } ,{
       id: 'nextjs',
       nom: 'ARAB',
       rasm: '/nextjs.jpg',
-      tavsif: ''
+      tavsif: '11:00 - 13:00 gacha'
     }, {
       id: 'nextjs',
       nom: 'ISPAN',
       rasm: '/nextjs.jpg',
-      tavsif: ''
+      tavsif: '14:00 - 16:00 gacha'
     }
   ]
   
--- app/page.js
+++ app/page.js
@@ -4,27 +4,49 @@ import { kurslar } from './data/kurslar'
 import Footer from './Footer/Footer';
 
 
+
 export default function Home() {
   return (
 
    <>
    <header>
-      <nav className="bg-white p-4">
-        <div className="container mx-auto">
+      <nav className="bg-gray-400 p-4 flex justify-between items-center shadow gap-2">
+        <span className="container mx-auto flex">
           <Link href="/" className="text-white text-lg font-bold">          <img className='w-30' src="4317.png" alt="" />
           </Link>
-        </div>
+        </span>
         <div className="container mx-auto flex justify-between items-center">
           <Link href="/" className="text-gray-800 text-lg font-bold">Bosh sahifa</Link>
           <Link href="/kurslar" className="text-gray-800 text-lg font-bold">O‘quv kurslari</Link>
           <Link href="/yangiliklar" className="text-gray-800 text-lg font-bold">Yangiliklar</Link></div>
+          <button className= 'text-1xl bg-gray-800 text-white px-4 py-2 rounded hover:bg-gray-700'>
+            connact
+
+          </button>
           
       </nav>
    </header>
+    <section className="bg-white p-6 flex" >
+      <div><h2 className='text-9xl font-bold text-center mt-10 text-indigo-500' >
+        Everest o‘quv kurslari
+
+      </h2>
+      <p>
+
+
+      </p>
+
+      </div>
+      <div>
+        <Image src="/4317.png" width={500} height={500} alt="Logo" className="w-1/2 h-auto mx-auto mt-10" />
+
+      </div>
+      
+    </section>
    
     <main className="p-6">
       <h1 className="text-3xl font-bold mb-6">O‘quv Kurslari</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
         {kurslar.map((kurs) => (
           <Link key={kurs.id} href={`/kurslar/${kurs.id}`}>
             <div className="border rounded p-4 hover:shadow-lg cursor-pointer">
